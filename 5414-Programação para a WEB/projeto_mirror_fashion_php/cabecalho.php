e86fb126751eadcdff4a34ee997e5fd94d752e79
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-10-24 09:19:37 +0100

    aula dia 23-10-2024

--- 5414-Programação para a WEB/projeto_mirror_fashion_php/cabecalho.php
+++ 5414-Programação para a WEB/projeto_mirror_fashion_php/cabecalho.php
@@ -2,28 +2,28 @@
 <html>
 
 <head>
-    <title><?php print $cabecalho_title; ?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <?php print @$cabecalho_css; ?>
-    <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+	<title><?php print $cabecalho_title; ?></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<?php print @$cabecalho_css; ?>
+	<link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
 </head>
 
 <body>
-    <header class="container">
-        <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
+	<header class="container">
+		<h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
 
-        <p class="sacola">Nenhum item na sacola de compras</p>
+		<p class="sacola">Nenhum item na sacola de compras</p>
 
-        <nav class="menu-opcoes">
-            <ul>
-                <li><a href="#">Sua Conta</a></li>
-                <li><a href="#">Lista de Desejos</a></li>
-                <li><a href="#">Cartão Fidelidade</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li><a href="#">Ajuda</a></li>
-            </ul>
-        </nav>
-    </header>
+		<nav class="menu-opcoes">
+			<ul>
+				<li><a href="#">Sua Conta</a></li>
+				<li><a href="#">Lista de Desejos</a></li>
+				<li><a href="#">Cartão Fidelidade</a></li>
+				<li><a href="sobre.php">Sobre</a></li>
+				<li><a href="#">Ajuda</a></li>
+			</ul>
+		</nav>
+	</header>
