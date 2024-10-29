fcd10aa95fba031774e713d5323818deeae730d0
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-10-29 09:31:34 +0000

    aula dia 28-10-2024

--- 5414-Programação para a WEB/projeto_mirror_fashion_php/checkout.php
+++ 5414-Programação para a WEB/projeto_mirror_fashion_php/checkout.php
@@ -6,6 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Checkout Mirror Fashion</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  <style>
+    .form-control:invalid {
+      border: 1px solid #c00;
+    }
+  </style>
 </head>
 
 <body>
@@ -17,26 +22,94 @@
       </p>
     </div> <!-- fim .container dentro do jumbotron -->
   </div> <!-- fim .jumbotron -->
+
   <div class="container">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h2 class="panel-title">Sua compra</h2>
-      </div> <!-- fim .panel-heading -->
-      <div class="panel-body">
-        <img src="img/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive">
-        <dl>
-          <dt>Produto</dt>
-          <dd><?= $_POST['nome'] ?></dd>
-          <dt>Preço</dt>
-          <dd><?= $_POST['preco'] ?></dd>
-          <dt>Cor</dt>
-          <dd><?= $_POST['cor'] ?></dd>
-          <dt>Tamanho</dt>
-          <dd><?= $_POST['tamanho'] ?></dd>
-        </dl>
-      </div> <!-- fim .panel-body -->
-    </div> <!-- fim .panel -->
-  </div><!-- fim .container da página -->
+    <div class="row">
+      <div class="col-sm-4 col-lg-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2 class="panel-title">Sua compra</h2>
+          </div> <!-- fim .panel-heading -->
+          <div class="panel-body">
+            <img src="img/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive hidden-xs">
+            <dl>
+              <dt>Produto</dt>
+              <dd><?= $_POST['nome'] ?></dd>
+              <dt>Preço</dt>
+              <dd><?= $_POST['preco'] ?></dd>
+              <dt>Cor</dt>
+              <dd><?= $_POST['cor'] ?></dd>
+              <dt>Tamanho</dt>
+              <dd><?= $_POST['tamanho'] ?></dd>
+            </dl>
+          </div> <!-- fim .panel-body -->
+        </div> <!-- fim .panel -->
+      </div>
+      <form class="col-sm-8 col-lg-9">
+        <div class="row">
+          <fieldset class="col-md-6">
+            <legend>Dados pessoais</legend>
+            <div class="form-group">
+              <label for="nome">Nome completo</label>
+              <input type="text" name="nome" id="nome" class="form-control" autofocus required>
+            </div>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <div class="input-group">
+                <span class="input-group-addon">@</span>
+                <input type="email" name="email" id="email" class="form-control" placeholder="email@exemplo.com">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="cpf">CPF</label>
+              <input type="text" name="cpf" id="cpf" class="form-control" placeholder="000.000.000-00" required>
+            </div>
+            <div class="checkbox">
+              <label>
+                <input type="checkbox" name="spam" value="sim" checked>
+                Quero receber spam da Mirror Fashion
+              </label>
+            </div>
+          </fieldset>
+          <fieldset class="col-md-6">
+            <legend>Cartão de Crédito</legend>
+            <div class="form-group">
+              <label for="numero-cartao">Número - CVV</label>
+              <input type="text" name="numero-cartao" id="numero-cartao" class="form-control">
+            </div>
+            <div class="form-group">
+              <label for="bandeira-cartao">Bandeira</label>
+              <select name="bandeira-cartao" id="bandeira-cartao" class="form-control">
+                <option value="master">Mastercard</option>
+                <option value="visa">VISA</option>
+                <option value="amex">American Express</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="validade-cartao">Validade</label>
+              <input type="month" name="validade-cartao" id="validade-cartao" class="form-control">
+            </div>
+          </fieldset>
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-thumbs-up"></span>
+          Confirmar Pedido
+        </button>
+      </form>
+    </div> <!-- fim .row -->
+  </div> <!-- fim .container da página -->
+
+  <script>
+    document.querySelector('input[type=email]').oninvalid = function() {
+      // remove mensagens de erro antigas
+      this.setCustomValidity("");
+      //reexecuta validação
+      if (!this.validity.valid) {
+        // se inválido, coloca mensagem de erro
+        this.setCustomValidity("Email inválido");
+      }
+    }
+  </script>
 </body>
 
 </html>
